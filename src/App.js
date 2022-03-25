b2baa0e1d8e9133d7706bd670778722b8ff3d1c3
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-03-25 19:06:01 +0600

    created multiple contexts

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import Navbar from "./components/Navbar";
 import Booklist from "./components/Booklist";
 import ThemeContextProvider from "./contexts/ThemeContext";
 import ToggleTheme from "./components/ToggleTheme";
+import AuthContextProvider from "./contexts/AuthContext";
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <Booklist />
-        <ToggleTheme />
+        <AuthContextProvider>
+          <Navbar />
+          <Booklist />
+          <ToggleTheme />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
