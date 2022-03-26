f59359a28b5441b7160da7d7231fae4c5b68a8f3
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-03-26 11:52:15 +0600

    Nested context in react

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 import { ThemeContext } from "../contexts/ThemeContext";
 export default class Navbar extends Component {
-  static contextType = ThemeContext;
-
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <ThemeContext.Consumer>
+            {(themeContext) => {
+              const { isAuthenticated, toggleAuth } = authContext;
+              const { isLightTheme, light, dark } = themeContext;
+              const theme = isLightTheme ? light : dark;
+              return (
+                <nav style={{ background: theme.ui, color: theme.syntax }}>
+                  <h1>Context App</h1>
+                  <div onClick={() => toggleAuth()}>
+                    {isAuthenticated ? "logged in" : "logged out"}
+                  </div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
